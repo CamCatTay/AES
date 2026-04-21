89fbb077b5dba2bc590b5cec3c670c467fd9e522
99rr <roy.hogrefe@gmail.com>
2026-04-21 00:10:55 -0400

    Update AES.java
    AES shift rows inverse, mix columns/inverse mix columns, hex print now functional (some tests included).

--- AES.java
+++ AES.java
@@ -2,6 +2,7 @@ public class AES {
   
   public static void main(String[] args){
     test_shift_rows();
+    test_mix_columns();
   }
   
   public static void shift_rows(int[][] block){
@@ -20,15 +21,15 @@ public class AES {
     }
   }
   
-  public static void inverse_shift_rows(int[][] block){ // causes error for now. will be fixed
+  public static void inverse_shift_rows(int[][] block){
     if (block.length!=4 || block[0].length!=4){
       System.out.println("shift_rows: invalid input block. Must be 4x4 (is " + block.length + "x" + block[0].length + "). Block unchanged");
     }
     for(int i = 1; i < block.length; i++){
-      int temp0 = block[i][i%4];
-      int temp1 = block[i][(-i+1)%4];
-      int temp2 = block[i][(-i+2)%4];
-      int temp3 = block[i][(-i+3)%4];
+      int temp0 = block[i][(-i+4)%4];
+      int temp1 = block[i][(-i+5)%4];
+      int temp2 = block[i][(-i+6)%4];
+      int temp3 = block[i][(-i+7)%4];
       block[i][0] = temp0;
       block[i][1] = temp1;
       block[i][2] = temp2;
@@ -38,56 +39,90 @@ public class AES {
   
   public static void test_shift_rows(){
     int[][] block = {
-      {1,5,3,10},
-      {4,11,1,8},
-      {15,3,14,2},
-      {7,8,6,9}
+      {0xf3,0x44,0x19,0x59},
+      {0x7b,0xa2,0x16,0x02},
+      {0xba,0xf1,0x11,0x0f},
+      {0x37,0x80,0x9d,0xcb}
     };
-    System.out.println("Block before shift_rows:");
+    System.out.println("Original block before shift_rows:");
     hex_print(block);
     shift_rows(block);
     System.out.println("Block after shift_rows:");
     hex_print(block);
-   int[][] block2 = {
-      {1,5,3,10},
-      {4,11,1,8},
-      {15,3,14,2},
-      {7,8,6,9}
-    };
-    inverse_shift_rows(block2);
-    System.out.println("Block after inverse_shift_rows:");
-    hex_print(block2);
+    inverse_shift_rows(block);
+    System.out.println("Block after inverse_shift_rows (should be original):");
+    hex_print(block);
   }
   
-  public static void mix_columns(int[][] block){ // a work in progress. Does nothing yet
+  public static void mix_columns(int[][] block){
     if (block.length!=4 || block[0].length!=4){
       System.out.println("shift_rows: invalid input block. Must be 4x4 (is " + block.length + "x" + block[0].length + "). Block unchanged");
     }
+    int mx = 0x11b;
     int[][] mix_constant = {
-      {2,3,1,1},
-      {1,2,3,1},
-      {1,1,2,3},
-      {3,1,1,2}
+      {0x2,0x3,0x1,0x1},
+      {0x1,0x2,0x3,0x1},
+      {0x1,0x1,0x2,0x3},
+      {0x3,0x1,0x1,0x2}
     };
+    for (int j = 0; j < block[0].length; j++){ // loop through columns
+      int[] temps = new int[block.length];
+      for (int i = 0; i < block.length; i++){ // loop through rows
+        temps[i] = GaloisMath.multiply_galois(mix_constant[i][0],block[0][j],mx) ^ GaloisMath.multiply_galois(mix_constant[i][1],block[1][j],mx) ^ GaloisMath.multiply_galois(mix_constant[i][2],block[2][j],mx) ^ GaloisMath.multiply_galois(mix_constant[i][3],block[3][j],mx);
+      }
+      block[0][j] = temps[0];
+      block[1][j] = temps[1];
+      block[2][j] = temps[2];
+      block[3][j] = temps[3];
+    }
   }
   
-  public static void inverse_mix_columns(int[][] block){ // a work in progress. Does nothing yet
+  public static void inverse_mix_columns(int[][] block){
     if (block.length!=4 || block[0].length!=4){
       System.out.println("shift_rows: invalid input block. Must be 4x4 (is " + block.length + "x" + block[0].length + "). Block unchanged");
     }
+    int mx = 0x11b;
     int[][] mix_constant = {
-      {14,11,13,9},
-      {9,14,11,13},
-      {13,9,14,11},
-      {11,13,14,14}
+      {0xe,0xb,0xd,0x9},
+      {0x9,0xe,0xb,0xd},
+      {0xd,0x9,0xe,0xb},
+      {0xb,0xd,0x9,0xe}
     };
+    for (int j = 0; j < block[0].length; j++){ // loop through columns
+      int[] temps = new int[block.length];
+      for (int i = 0; i < block.length; i++){ // loop through rows
+        temps[i] = GaloisMath.multiply_galois(mix_constant[i][0],block[0][j],mx) ^ GaloisMath.multiply_galois(mix_constant[i][1],block[1][j],mx) ^ GaloisMath.multiply_galois(mix_constant[i][2],block[2][j],mx) ^ GaloisMath.multiply_galois(mix_constant[i][3],block[3][j],mx);
+      }
+      block[0][j] = temps[0];
+      block[1][j] = temps[1];
+      block[2][j] = temps[2];
+      block[3][j] = temps[3];
+    }
   }
   
-  public static void hex_print(int[][] block){// prints in base 10 for now. should eventually print in hex
+  public static void test_mix_columns(){
+    int[][] block = {
+      {0x87,0xf2,0x4d,0x97},
+      {0x6e,0x4c,0x90,0xec},
+      {0x46,0xe7,0x4a,0xc3},
+      {0xa6,0x8c,0xd8,0x95}
+    };
+    System.out.println("Original Block before mix_columns:");
+    hex_print(block);
+    mix_columns(block);
+    System.out.println("Block after mix_columns:");
+    hex_print(block);
+    inverse_mix_columns(block);
+    System.out.println("Block after inverse_mix_columns (should be original):");
+    hex_print(block);
+  }
+  
+  // prints an int[][] array in hex
+  public static void hex_print(int[][] block){
     System.out.println("{");
     for(int i = 0; i < block.length; i++){
       for(int j = 0; j < block[0].length; j++){
-        System.out.print(block[i][j] + ", ");
+        System.out.print(Integer.toHexString(block[i][j]) + ", ");
       }
       System.out.println();
     }
